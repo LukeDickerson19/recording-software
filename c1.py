ca5dfd4f8d97c838b06dba0c202ff8ec30ca0bad
PopeyedLocket <lucius.dickerson@gmail.com>
2019-06-26 11:49:59 -0700

    created bit_crushing distortion effect

--- c1.py
+++ c1.py
@@ -4,34 +4,79 @@
 
         NOW:
 
-            get distortion effect
-                https://www.hackaudio.com/digital-signal-processing/distortion-effects/
-                http://sites.music.columbia.edu/cmc/music-dsp/FAQs/guitar_distortion_FAQ.html
-                https://ccrma.stanford.edu/~jos/pasp05/Nonlinear_Distortion.html
-                http://people.math.sfu.ca/~cbm/aands/
+            test bit_crushing
 
-            fix it so sound comes from both headphone ears
-                why is the data in the right headphone static?
+            rename a,b,c:1,2 files to make more sense
 
-            plot the left and right head phones over time
-            make the axes show the duration over time
-                (maybe a second)
+            update backup
 
-            the frequency chart (aka spectrogram)
+            push it all to github
 
-            maybe make a backup of all this first
+            find a way to record
+                that doesn't create lag over time
+                np.concatinate() didn't work
+                vvv    this explains why    vvv
+                https://stackoverflow.com/questions/38470264/numpy-concatenate-is-slow-any-alternative-approach
+                its because its copying over the whole array each time
+                    in what other senarios is numpy copying shit unneccessarily?
+                try a python list.append()
+                if that doesn't work (or affets latency in any noticable way) use a linked list
 
-            figure out what 'data' numpy array represents
-                plot (net) volume over time
-                frequency
-                    make each frequency appear on a plot that has
-                        frequency on the y axis
 
-                    see spectrogram.py
 
         LONG TERM:
 
+
             set up pedal
+                probably just need to create another InputStream w/ sounddevice with a callback function (does InputStream have a callback function? If not, gotta find some way to access the raw data)
+                In the callback function (or whatever data stream) there will be a boolean thats constantly being updated, find a way to interpret that to set another global boolean to turn distortion on/off
+
+
+            setup 2i2 to work with a mic and a guitar
+                why does it say the 2i2 only has 2 channels? shouldn't it be 4?
+
+                for now just use computer's mic (and create a 2nd sd.Stream)
+
+
+            right now sd.default.latency = 'low' in order to decrease latency.
+                it does this by decreasing the length of the numpy array 'indata' (from 512 to 128)
+                which decreases the quality of the sound (idk why)
+
+                I want to find a way to decrease latency without decreasing sound quality as much.
+                Possible solutions are:
+
+                    use:
+                        multiprocessing or async-io to do more shit in parallel hopefully
+                            https://realpython.com/async-io-python/#the-10000-foot-view-of-async-io
+
+                        research sd.default.latency (maybe theres a way to set it to 'medium low' or something to put it at 256, maybe 256 wont have noticable latency and quality will be pretty good too)
+
+                        maybe it could just do linear interpolation of the 128 indata and put it back to 512 (or 1024 lol XD)
+                            or maybe something smoother than linear interpolation
+
+
+            make basic frequency chart (aka spectrogram)
+                no Key interpretation
+                see spectrogram.py
+
+
+            figure out what 'indata' numpy array represents
+                create a new script thats just an OutputStream (to headphones)
+                create numpy arrays that are a sine wave with specific note's freqency (middle C)
+                plot it over time
+                see if the pitch in the headphones matches the pitch on your guitar (don't forget to tune it)
+                dig into spectogram.py to figure out how it determines frequency value
+
+
+            figure out why right headphone data is just static
+                right now the hack is simply to copy the left-headphone data into the right
+                however from testing it by downloading classical_music.wav files from online
+                and playing it through the headphones and outputting the data to the console,
+                its clear the data should not be the same in both head phones
+
+                perhaps plot the left and right data on the same plot (one line red, one line blue)
+                and see their difference, maybe they're not that different and you just need to
+                randomly change the right one a bit from the left to get rid of that chorus sound
 
 
     THOUGHTS:
@@ -51,7 +96,19 @@
         It would be cool if the Program could figure out what time signature you're playing
         in by simply listening to you play
             as a musician your time signature is going to fluctuate slightly ...
-            and you could set it to either
+            and you could set it to either go with it (because maybe you want the song to speed up or slow down)
+            or you could tell it to stick with whatever time signature it first identified (to keep you the musician on track)
+
+            it would be nice if this was then played back in the headphones or displayed with a blinking light on the pedal or with some display on the computer screen
+
+            this could be useful for
+            having the computer properly time the switch from 1 effect to another (triggered with foot pedal), thus allowing the musician to press the pedal slightly before the
+
+        It would be cool if it could Identify what key you are playing in and
+        display horizontal lines on the screen for each note in the key
+            and when you stop playing the lines go away
+
+            ... having horizontal and vertical lines (for key and time sig. respectivewly) appear on a black screen when you start playing and disappearing when you stop would be dope!!!!
 
         It would be cool if you could pre-set the algorithm to apply various
         effects at various points in the song (identified with AI)
@@ -69,16 +126,11 @@ np.set_printoptions(threshold=10)
 import sounddevice as sd
 sd.default.latency = 'low'
 plotdata = np.zeros(128)
-
-SAMPLERATE = 44100
-WINDOW = 200
-DOWNSAMPLE = 10
-INTERVAL = 30
-CHANNELS = [1]
-length = int(WINDOW * SAMPLERATE / (1000 * DOWNSAMPLE))
 # print(plotdata)
 # print(plotdata.shape)
 # sys.exit()
+
+SAMPLERATE = 44100
 
 
 # mapping = [c - 1 for c in CHANNELS]  # Channel numbers start with 1
@@ -88,9 +140,12 @@ wav2 = 'wav2.wav'
 all_data = np.array([[0.0, 0.0]])
 
 cubic = lambda x : x - (1/3)*(x**3)
+
 alpha = 75
 arctan = lambda x : (2 / np.pi) * np.arctan(alpha * x)
-# 2 / pi * arctan(alpha*input)
+
+threshold = 0.0005
+bit_crushing = lambda x : threshold if x > threshold else (-threshold if x < -threshold else 0)
 
 # def audio_callback(indata, frames, time, status):
 def audio_callback(indata, outdata, frames, time, status):
@@ -156,8 +211,13 @@ def audio_callback(indata, outdata, frames, time, status):
     # bit crushing
     # "round signal to -1, 0, or 1 (or any combo you want)" (para-phrased)
     #    - https://www.hackaudio.com/digital-signal-processing/distortion-effects/bit-crushing/
-
-
+    amplitude = 500.0
+    left_indata = indata[:,0][:,np.newaxis]
+    outdata[:] = amplitude * \
+        np.repeat(
+            bit_crushing(
+                left_indata),
+            2, 1)
 
     # print('outdata')
     # print(outdata.shape)
